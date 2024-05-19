4b2e4a6779f4efdda12da8f6c55088e6b952c43f
Mariam <mariam.yasser772000@gmail.com>
2024-05-19 15:16:48 -0700

    states update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -80,5 +80,5 @@ class DBStorage:
         self.__session = Session()
 
     def close(self):
-        """call remove() method on the private session attribute"""
-        self.__session.remove()
+        """call colse() method on the private session attribute"""
+        self.__session.close()
