129b29fb86d855440c53e12f9d70ddb085c9d172
Mariam <mariam.yasser772000@gmail.com>
2024-05-19 14:18:27 -0700

    flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-""" starts a Flask web application """
+""" Sript that starts a Flask web application """
 
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """Returns Hello HBNB!"""
-    return 'Hello HBNB!'
+def hello_hbn():
+    """ Return Hello HBNB!"""
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Returns HBNB"""
-    return 'HBNB'
+    """Return HBNB"""
+    return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
-    return 'C ' + text.replace('_', ' ')
+def text_var(text):
+    """Display text variable passed in"""
+    return "C {}".format(text.replace("_", " "))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+        app.run(host='0.0.0.0', port=5000)
